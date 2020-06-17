654cc3bf0b03cc0e50ec6f171d94fbb99bbe156a
Arturo Aguilar Lopez <aguilarlopez18@gmail.com>
2020-06-17 16:30:40 -0300

    First commit

---
+++ hash.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash.h"
+
+struct hash{
+    size_t cantidad;
+
+};
+
+hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
+    hash_t * hash = malloc(sizeof(hash_t));
+    if(!hash)   return NULL;
+
+
+
+    return hash;
+}
+
