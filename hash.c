2004b2c642f9ca0bd246ef04e52ee7ddcc7c56de
Sebastian Soto <sebasoto98@gmail.com>
2020-06-18 19:51:37 -0300

    cambios

--- hash.c
+++ hash.c
@@ -15,6 +15,7 @@ typedef enum{
 }estado_t;
 
 typedef struct{
+
     char *clave;
     void *dato;
     estado_t estado;
@@ -23,22 +24,51 @@ typedef struct{
 struct hash{
     size_t cantidad;
     size_t capacidad;
-    hash_campo_t *campo;
+    hash_campo_t *tabla;
     hash_destruir_dato_t destruir_hash;
 };
 
 hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
     hash_t * hash = malloc(sizeof(hash_t));
-    if(!hash)   return NULL;
+    if(!hash)   
+    	return NULL;
 
     hash->capacidad = TAM_HASH_INICIAL;
     hash->cantidad = 0;
 
+    hash -> tabla = malloc(sizeof(hash_campo_t)*hash-> capacidad);
+    if(!hash -> campo){
+    	free(hash);
+    	return NULL;
+    }
 
     return hash;
 }
 
 bool hash_guardar(hash_t *hash, const char *clave, void *dato){
+	hash_campo_t * campo = malloc(sizeof(hash_campo_t));
+	if(!campo)
+		return false;
 
 	
+
+	campo -> clave = clave;
+	campo -> dato = dato;
+	campo -> estado = OCUPADO;
+
+	int indice = f(clave) % hash -> capacidad;
+	
+	hash[indice] = campo;
+
+	return true;
+
+}
+
+size_t hash_cantidad(const hash_t * hash){
+	return hash->cantidad;
+}
+
+bool hash_borrar (hash_t * hash,const char * clave){
+
+
 }
