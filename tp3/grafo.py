269cd22a1eca29e21a0af22f1c6c83e93a428fc2
Arturo Aguilar Lopez <aguilarlopez18@gmail.com>
2020-07-29 19:33:01 -0300

    probando python

--- tp3/grafo.py
+++ tp3/grafo.py
@@ -3,13 +3,69 @@ TDA Grafo
 '''
 
 class Grafo:
-    def __init__(self):
+    def __init__(self, es_nodirigido = False):
         self.vertices = {}
-
-
-    def agregar_vertice()
-
-
-    def agregar_arista()
-
+        self.es_nodirigido = es_nodirigido
+        self.cant_vertices = 0
     
+
+    def agregar_vertice(self, vertice, dato = None):
+        if vertice not in self.vertices:
+            self.vertices[vertice] = {}
+            self.cant_vertices += 1
+        else: #Solo para probar el grafo, despues habria que borrar este else
+             print("Vertice: '%s' ya existe" % str(clave_vertice))
+    
+    def borrar_vertice(self, vertice):
+        if vertice not in self.vertices:
+            return False
+        #del self.vertices[vertice]
+        for w in self.vertices.values():
+            if vertice in w:
+                w.pop(vertice)
+        del self.vertices[vertice]
+        self.cant_vertices -= 1
+
+
+    def agregar_arista(self, v1, v2, peso):
+        if v1 not in self.vertices or v2 not in self.vertices:
+            return False
+        self.vertices[v1][v2] = peso
+
+
+    def borrar_arista(self, arista)
+
+    def adyacentes(self, vertice):
+
+    def vertice_en_grafo(self, vertice)
+
+    def obtener_vertice(self, vertice):
+        if vertice in self.vertices:
+            return self.vertices[vertice]
+        else:
+            return None
+    
+    def vertice_pertenece(self, vertice):
+        return vertice in self.vertices
+
+
+
+    def __iter__(self):
+        return iter(self.vertices)
+
+'''
+Primitivias: Lo saque de la lista, fue una respuesta de Martin
+- Crear -> el init OK
+- Destruir (si corresponde) -> En python creo que no hace falta destuir
+- Agregar/Borrar un vértice -> agregar_vertice OK
+- Agregar/Borrar una arista
+- Ver si un vértice está en el grafo. -> vertice_pertenece OK 
+- Si los vértices pudieran tener más información asociada, ver obtener dicha información (eso depende de la implementación, porque podría dejarse eso como algo externo). 
+- Ver si dos vértices están conectados. 
+- Obtener el peso de la unión de dos vértices (si fuera no pesado, podría ser 1). 
+- Obtener todos los vértices
+- Obtener un vértice aleatorio 
+- Obtener los adyacentes a un vértice. 
+- Obtener la cantidad de vértices.(len del grafo?)
+- Tener alguna forma de iterarlo. -> con funcion iter de python
+'''
--- tp3/netstats.py
+++ tp3/netstats.py
@@ -2,19 +2,46 @@
 
 import random
 import sys
-import grafo
+
 import biblioteca
 
+
+graph = {
+    'A' : {'B' : '', 'C': ''},
+    'B' : {'A' : '4', 'D': '8'},
+    'C' : {'D': '0'},
+    'D' : {'B': '2'}
+}
 
 
 def main():
     Dicc=set(["ArturoAguilar","Hola","Buenas","Dijkstra"])
     d = 0
     for d in Dicc:
-        if Dicc[d] == "Buenas":
-            print("Encontre el Buenas")
+        if "Buenas" in Dicc:
+            print("El buenas esta en dicc")
+            break
         else:
             print(d)
+    
+    #for clave in graph.items():
+        #elemenetos = ",".join(valor.keys())
+#    print(graph.keys())
+ #   print(graph.values())
+  #  print(graph.items())
+    print(len(graph))
+    if 'C' in graph:
+        del graph['C']
+    print(len(graph))
+    print(graph.keys())
+    graph['Z'] = {'A':' '}
+    print(graph.keys())
+    print(graph.items())
+    for v in graph.values():
+        if 'B' in v:
+            v.pop('B')
+    print(graph.keys())
+    print(graph.items())
 
 #if __name__ == "__main__":
 main()
