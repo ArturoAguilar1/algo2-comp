0d41cd9a9e3c39c9c7fb465245e3c57079553079
Arturo Aguilar Lopez <aguilarlopez18@gmail.com>
2020-08-05 13:47:45 -0300

    actualizo

--- tp3/biblioteca.py
+++ tp3/biblioteca.py
@@ -26,11 +26,33 @@ def camino_minimo_bfs(grafo, origen, destino):
                 q.append(w)
     return padres, orden
 
+def bfs_rango(grafo, origen, n):
+    visitados = set()
+    padres = {}
+    orden = {}
+    q  = deque()
+    padres[origen] = None
+    orden[origen] = 0
+    visitados.add(origen)
+    q.append(origen)
+    while q:
+        v = q.popleft()
+        #print(v)
+        for w in grafo.adyacentes(v):
+            if w not in visitados:
+                visitados.add(w)
+                padres[w] = v
+                orden[w] = orden[v] + 1
+                q.append(w)
+    return padres, orden
+
 
 def caminos_minimos(grafo, origen):
     q = deque()
     visitados = set()
     distancia = {}
+    padres = {}
+    padres[origen] = None
     distancia[origen] = 0
     visitados.add(origen)
     q.append(origen)
@@ -42,19 +64,26 @@ def caminos_minimos(grafo, origen):
                 distancia[w] = distancia[v] + 1
                 q.append(w)
                 visitados.add(w)
-    return distancia
+                padres[w] = v
+    return distancia, padres
 
 def diametro(grafo):
     max_min_dist = 0
+    aux_distancias = {}
+    aux_padres = {}
     for v in grafo:
-        distancias = caminos_minimos(grafo, v)
+        distancias, padres = caminos_minimos(grafo, v)
         for w in distancias:
             if distancias[w] > max_min_dist:
                 max_min_dist = distancias[w]
-    return max_min_dist , distancias
+                aux_distancias = distancias
+                aux_padres = padres
+                aux_origen = v
+    return max_min_dist, aux_distancias, aux_padres, aux_origen
 
 
-def cfcs(grafo, v, apilados,mb, visitados, todas_cfc, orden):
+
+def cfcs_conectividad(grafo, v, apilados,mb, visitados, todas_cfc, orden):
     pila = deque()
     visitados.add(v)
     mb[v] = orden[v]
@@ -65,7 +94,7 @@ def cfcs(grafo, v, apilados,mb, visitados, todas_cfc, orden):
         if w not in visitados:
         #print(f'{w} no esta visitado, lo visito')
             orden[w] = orden[v] + 1
-            cfcs(grafo, w, apilados, mb, visitados, todas_cfc, orden)
+            cfcs_conectividad(grafo, w, apilados, mb, visitados, todas_cfc, orden)
 
     if w in apilados:
       if mb[v] > mb[w]:
@@ -85,3 +114,26 @@ def cfcs(grafo, v, apilados,mb, visitados, todas_cfc, orden):
     #print('NUEVA CFC!: ', nueva_cfc)
     todas_cfc.append(nueva_cfc)
   #print(f"Termine de trabajar con {v}")
+
+
+def reconstruir_ciclo(sol_parcial, inicio):
+    camino = []
+    camino = sol_parcial[:]
+    camino.append(inicio)
+    return camino
+
+def dfs_ciclo(grafo, v, buscado, visitados, sol_parcial, n):
+    for w in grafo.adyacentes(v):
+        if len(sol_parcial) > n:
+            return None
+        if w in visitados:
+            if w == buscado and len(sol_parcial) == n:
+                return reconstruir_ciclo(sol_parcial, buscado)
+        else:
+            visitados.add(w)
+            sol_parcial.append(w)
+            ciclo = dfs_ciclo(grafo, w, buscado, visitados, sol_parcial, n)
+            visitados.remove(sol_parcial.pop())
+            if ciclo is not None:
+                return ciclo
+    return None
--- tp3/netstats.py
+++ tp3/netstats.py
@@ -5,6 +5,7 @@ import sys
 from collections import deque
 from grafo import Grafo
 import biblioteca
+import json
 
 def listar_operaciones():
     for cmd in dicc_comandos.keys():
@@ -29,26 +30,79 @@ def camino(grafo, origen, destino):
     camino = []
     while pila:
         camino.append(pila.pop())
-    return camino, orden[destino]
+    x = ' '
+    print("Costo:"+x+str(orden[destino]))
+    #return camino, orden[destino]
 
 def mas_importantes():
     print("mas importantes")
 
 #Complejidad: O(P+L)
-def conectados(grafo, origen):
-    visitados = set()
-    apilados = set()
-    todas_cfc = []
-    orden = dict()
-    mb = dict()
-    biblioteca.cfcs(grafo, origen, apilados, mb, visitados, todas_cfc, orden)
-    print(todas_cfc)
-
 
 def diametro(grafo):
-    costo, distancias = biblioteca.diametro(grafo)
-    print(distancias)
-    print(costo)
+    costo, distancias, padres, aux_origen = biblioteca.diametro(grafo)
+    with open("text.text",'w') as fp:
+        print(distancias, file = fp)
+    fp.close()
+    aux_final = list(distancias.keys())[list(distancias.values()).index(costo)]
+    pila = deque()
+    aux = aux_final
+    while aux != aux_origen:
+        pila.append(aux)
+        aux = padres[aux]
+
+    pila.append(aux_origen)
+    camino = []
+    while pila:
+        camino.append(pila.pop())
+    #'->'.join(map(str,camino))
+    print(*camino, sep=' -> ')
+    #pila = deque()
+    #print(distancias)
+    x = ' '
+    print("Costo:"+x+str(costo))
+
+def ciclo_n(grafo, origen, n):
+    if not grafo.vertice_pertenece(origen):
+        print("no existe el vertice")
+        return None
+    sol_parcial = []
+    visitados = set()
+    sol_parcial.append(origen)
+    visitados.add(origen)
+    ciclo = biblioteca.dfs_ciclo(grafo, origen, origen, visitados, sol_parcial, n)
+    if ciclo == None:
+        print("No se encontro el recorrido")
+    else:
+        print(ciclo)
+
+
+def rango(grafo, origen, n):
+    padres, orden = biblioteca.bfs_rango(grafo, origen, n)
+    cont = 0
+    for v in orden:
+        if orden[v] == n:
+            cont += 1
+    return cont
+
+def navegacion(grafo, origen):
+    if grafo.vertice_pertenece(origen) == False:
+        return "No existe el origen en NetStats"
+    recorrido = []
+    recorrido.append(origen)
+    aux = list(grafo.adyacentes(origen))
+    n = 0
+    #aux is not None and
+    while n < 20:
+        try:
+            recorrido.append(aux[0])
+            aux = list(grafo.adyacentes(aux[0]))
+        except IndexError:
+            aux = 'null'
+            break
+        n += 1
+    print(*recorrido, sep=' -> ')
+
 
 def netstats_crear(ruta_archivo,grafo):
     datos = []
@@ -65,19 +119,50 @@ def netstats_crear(ruta_archivo,grafo):
 
     wiki.close()
 
+def grados_salida_dirigido(grafo):
+    grados = {}
+    for v in grafo.obtener_todos_vertices():
+        grados[v] = len(grafo.adyacentes(v))
+
+    return grados
+
+def conectados(grafo, origen):
+    return biblioteca.cfc_tarjan(grafo)
+    #print(todas_cfc)
+
 def main():
     if len(sys.argv) != 2:
         print("Cantidad de parametros inválidos")
-    
+    sys.setrecursionlimit(5000)
     grafo = Grafo(True)
     netstats_crear(sys.argv[1],grafo)
-    diametro(grafo)
+    #diametro(grafo)
+    #aux = list(grafo.adyacentes("Brasil"))
+    #print(aux[0])
+    navegacion(grafo, "Argentina")
+    navegacion(grafo, "Alemania")
+    navegacion(grafo, "queso")
+    navegacion(grafo, "Bolivia")
+    navegacion(grafo, "Brasil")
+
+
+
+    #print(rango(grafo, "Perón", 4))
+    #cfcs = conectados(grafo,"Boca Juniors")
+    #with open("text.txt",'w') as f:
+    #    print(cfcs, file=f)
+    #f.close()
+    #print(cfcs)
+    #print("Roma" in grafo.adyacentes("Japón"))
+    #print(grafo.adyacentes("Japón"))
+    #diametro(grafo)
+    #ciclo_n(grafo,"Rock de Argentina",2)
+    #print(grafo.adyacentes("Arte"))
     """cam, costo = camino(grafo, "Boca Juniors", "Australia")
-    print(cam)
-    x = ' '
-    print("Costo:"+x+str(costo))"""
+    print(cam)˙
+
     #conectados(grafo, "Boca Juniors")
-    #biblioteca.bfs(grafo,"A")
+    #biblioteca.bfs(grafo,"A")"""
     """print(grafo.vertices_cantidad())
     print(grafo.vertice_aleatorio())
     print(grafo.adyacentes("A"))
@@ -105,7 +190,10 @@ dicc_comandos = {
     'camino': camino,
     'mas_importantes': mas_importantes,
     'conectados': conectados,
-    'diametro': diametro
+    'diametro': diametro,
+    'ciclo': ciclo_n,
+    'rango': rango,
+    'navegación': navegacion
 }
 
 main()
